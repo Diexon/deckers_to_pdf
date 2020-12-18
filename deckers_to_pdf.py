48726db8b3e551932541e8fb73b7193c8d8ce654
Diego Gil <diego.gilquez@gmail.com>
2020-12-18 18:52:52 +0100

    Refine cards namig. Introduce input.

--- deckers_to_pdf.py
+++ deckers_to_pdf.py
@@ -1,4 +1,3 @@
-
 from bs4 import *
 import requests as rq
 import os
@@ -19,9 +18,10 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# Parse decker's deck
+deck = input("Enter link to the deck: ")
 
-
-deck = "https://www.underworlds-deckers.com/en/deck-builder/mollogs-mob/?ObjectiveCard=2206,5261,5263,5278,5288,5301,5307,8259,8262,8287,8315,8326,&GambitCard=2213,2218,5323,5324,5341,5366,5377,6017,7017,8363,&UpgradeCard=2223,2224,2226,2228,5393,5396,5398,5413,5438,7039,&DeckTitle=Mollog%20competitivo%202"
+# For test: https://www.underworlds-deckers.com/en/deck-builder/mollogs-mob/?ObjectiveCard=2206,5261,5263,5278,5288,5301,5307,8259,8262,8287,8315,8326,&GambitCard=2213,2218,5323,5324,5341,5366,5377,6017,7017,8363,&UpgradeCard=2223,2224,2226,2228,5393,5396,5398,5413,5438,7039,&DeckTitle=Mollog%20competitivo%202
 
 deck_title = unquote(re.search('DeckTitle=(.*)', deck).group(1))
 # Check if deck is accessible
@@ -40,17 +40,18 @@ folder_out = deck_title.replace(' ','_')
 if not os.path.exists(folder_out):
     os.mkdir(folder_out)
 
-for card_type in card_types: 
+for type_idx, card_type in enumerate(card_types): 
     print("Scrapping " + card_type + "...")
     pattern = card_type + "=(.*?),\&"
     card_numbers = re.search(pattern, deck).group(1).split(',')
     # Safe the cards
-    for card_number in card_numbers:
+    for idx, card_number in enumerate(card_numbers):
         img_data = rq.get('https://www.underworlds-deckers.com/imagesBackground6/1/'+card_number+'.png')
         if img_data.status_code != 404:
             img_data = img_data.content
             print(bcolors.OKGREEN + "\t" + card_number + " -> Downloaded" + bcolors.ENDC)
-            with open(folder_out + "/" + card_number + ".png", 'wb+') as f:
+            image_path = os.path.join(folder_out, str(type_idx) + '_' + card_type + '_' + str(idx) + '_' + card_number + ".png")
+            with open(image_path, 'wb+') as f:
                 f.write(img_data)
         else:
             print(bcolors.FAIL + bcolors.BOLD + "\tERROR: " + card_number + " -> Not accessible" + bcolors.ENDC)
